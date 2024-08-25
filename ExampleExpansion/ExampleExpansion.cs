6b5c824e1425f6647bf300d84dce1e2ea36f2287
NotZer0Two <lyonfan2010@gmail.com>
2024-08-25 22:29:40 +0200

    Aggiungere i file di progetto.

---
+++ ExampleExpansion/ExampleExpansion.cs
@@ -0,0 +1,27 @@
+﻿using Exiled.API.Features;
+using PlaceholderAPI.API.Abstract;
+
+namespace ExampleExpansion
+{
+    public class ExampleExpansion : PlaceholderExpansion
+    {
+        public override string Author { get; set; } = "NotZer0Two";
+        public override string Identifier { get; set; } = "example";
+        public override string RequiredPlugin { get; set; } = null;
+
+        public override string OnRequest(Player player, string param)
+        {
+            return $"Hello, {player.Nickname}";
+        }
+
+        public override string OnOfflineRequest(string param)
+        {
+            if(param == "notsad")
+            {
+                return "Im happpy :)";
+            }
+
+            return "No Friends :(";
+        }
+    }
+}
